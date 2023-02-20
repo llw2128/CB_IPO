d10bddb9ea0b2da4c6cb566e83e29a3aa6ca97ce
Leon Wu <leonwu@Leons-MacBook-Air-2.local>
2023-02-19 23:23:19 -0500

    added untested functions for crunchbase, created funciton to generate dataframe from scraped data

--- CB_IPO.py
+++ CB_IPO.py
@@ -6,38 +6,72 @@ import numpy as np
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 
-#API_KEY = "99d839ea83f2e31b7b116a03f51087fada8ac3ef32abaa8e7d702b1d037487fc"
 
-#queryApi = QueryApi(api_key=API_KEY)
+#queryApi = QueryApi(api_key=API_KEY) 
+
 browser =  webdriver.Chrome('./chromedriver')
 
 url_info = "https://www.sec.gov/edgar/search/#/filter_forms=S-1"
 
+"""
+Resets url for accessing pages of EDGAR table
+"""
 
+def set_page(pNo):
+  pstr = '&page={}'.format(pNo)
+  global url_info
+
+  i = url_info.find('&page=')
+  if i<0:
+    url_info+=pstr
+    print(url_info)
+  else:
+    url_info = url_info[:i]+pstr
+    print(url_info)
+    
+#Helper funciton to modify dates for webscraper
 def set_search_date(d1,d2):
   dates = 'dateRange=custom&category=custom&startdt={}&enddt={}&'.format(d1,d2)
   global url_info 
   url_info= "https://www.sec.gov/edgar/search/#/{}filter_forms=S-1".format(dates)
 
+
 def edgar_scrape():
-  e_data = []
+  c_names = []
   sada = browser.get(url_info)
   source = browser.page_source
   html_s = bs(source, 'html.parser')
 
+  #find name for all recent S-1 filers with the  SEC
   for item in html_s.findAll( attrs={'class': 'entity-name'}):
     if item.text != 'Filing entity/person':
-      e_data.append([item.text])
+      c_names.append(item.text)
 
-  i =0
+  #generate list of the  filing dates 
+  c_dates = []
   for item in html_s.findAll( attrs={'class': 'filed'}):
     if item.text != 'Filed':
-      e_data[i].append(item.text)
-      i+=1
-  print(e_data)
+      c_dates.append(item.text)
 
-def generate_df(comp_arr):
+  #print(c_names)
+  return (c_names,c_dates)
+
+#argument is the number of pages to be pulled using scraper, deafult 1
+def generate_df(num_page=1):
+  ns,ds = edgar_scrape()
+  d = {'names':ns, 'filing date':ds}
   
+  if num_page>1:
+    #adds values to dictionary from queried pages
+    for i in range(num_page-1):
+      set_page(i+2)
+      ns2,ds2 = edgar_scrape()
+      d['names']+=ns2
+      d['filing date']+=ds2
+
+  df = pd.DataFrame(data=d)
+  print(df)
+  return  df
 
 def get_ipo():
 
@@ -49,9 +83,25 @@ def get_ipo():
   "size": "10",
   "sort": [{ "filedAt": { "order": "desc" } }]}
   #filings = q.get_filings(query)
-  #print(filings)
+
+########
+def search_CB(name):
+  CB_key = input('Enter CB API: ')
+  req = 'https://api.crunchbase.com/api/v4/autocompletes?query={}&limit=15&user_key={}'.format(name,CB_key)
+  CB_info =  requests.get(req).text
+  print(CB_info)
+  return(CB_info[0])
+
+def add_CB(dframe):
+  gen_descr = []
+
+  for r in dframe.rows:
+    gen_descr.append(search_CB(r['names']))
+  dframe['CB Description'] = gen_descr
+
 
 #set_search_date('2018-02-20','2023-02-10')
 #print(url_info)
-edgar_scrape()
+generate_df(3)
+
 
