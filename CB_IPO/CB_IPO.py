d2907f7813b81b0a4c460771653e9786964d8dd7
Leon Wu <leonwu@Leons-MacBook-Air-3.local>
2023-03-10 22:45:29 -0500

    added CI/CD

--- CB_IPO/CB_IPO.py
+++ CB_IPO/CB_IPO.py
@@ -1,15 +1,16 @@
 import requests
-import sys
-import pandas as pd 
-import numpy as np 
-#from sec_api import QueryApi
+import pandas as pd
+
+# import numpy as np
+
+# from sec_api import QueryApi
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 
 
-#queryApi = QueryApi(api_key=API_KEY) 
+# queryApi = QueryApi(api_key=API_KEY)
 
-browser =  webdriver.Chrome('./chromedriver')
+browser = webdriver.Chrome('./chromedriver')
 
 url_info = "https://www.sec.gov/edgar/search/#/filter_forms=S-1"
 
@@ -17,131 +18,158 @@ url_info = "https://www.sec.gov/edgar/search/#/filter_forms=S-1"
 Resets url for accessing pages of EDGAR table
 """
 
-def set_page(pNo):
-  pstr = '&page={}'.format(pNo)
-  global url_info
 
-  i = url_info.find('&page=')
-  if i<0:
-    url_info+=pstr
-    print(url_info)
-  else:
-    url_info = url_info[:i]+pstr
-    print(url_info)
-  return url_info
-    
+def set_page(pNo):
+    pstr = '&page={}'.format(pNo)
+    global url_info
+
+    i = url_info.find('&page=')
+    if i < 0:
+        url_info += pstr
+        print(url_info)
+    else:
+        url_info = url_info[:i] + pstr
+        print(url_info)
+    return url_info
 
 
 def reset_url():
-  global url_info
-  url_info = "https://www.sec.gov/edgar/search/#/filter_forms=S-1"
-  return url_info
+    global url_info
+    url_info = "https://www.sec.gov/edgar/search/#/filter_forms=S-1"
+    return url_info
 
-#Helper funciton to modify dates for webscraper
-def set_search_date(d1,d2):
-  dates = 'dateRange=custom&category=custom&startdt={}&enddt={}&'.format(d1,d2)
-  global url_info 
-  url_info= "https://www.sec.gov/edgar/search/#/{}filter_forms=S-1".format(dates)
-  return url_info
+
+# Helper funciton to modify dates for webscraper
+def set_search_date(d1, d2):
+    dates = 'dateRange=custom&category=custom&startdt={}&enddt={}&'.format(d1, d2)
+    global url_info
+    url_info = "https://www.sec.gov/edgar/search/#/{}filter_forms=S-1".format(dates)
+    return url_info
 
 
 def edgar_scrape(num):
-  c_names = []
-  c_dates = []
-  forms = set()
-  sada = browser.get(url_info)
-  source = browser.page_source
-  html_s = bs(source, 'html.parser')
+    c_names = []
+    c_dates = []
+    form_type = set()
+    browser.get(url_info)
+    source = browser.page_source
+    html_s = bs(source, 'html.parser')
 
-  #find name for all recent S-1 filers with the  SEC
-  i =0
-  for item in html_s.findAll( attrs={'class': 'entity-name'}):
-    if i == num:
-      break
-    if item.text != 'Filing entity/person':
-      c_names.append(item.text)
-      i+=1
+    # find name for all recent S-1 filers with the  SEC
+    i = 0
+    for item in html_s.findAll(attrs={'class': 'entity-name'}):
+        if i == num:
+            break
+        if item.text != 'Filing entity/person':
+            c_names.append(item.text)
+            i += 1
 
-  #generate list of the  filing dates 
-  
-  i2 =0
-  for item in html_s.findAll( attrs={'class': 'filed'}):
-    if i2 == num:
-      break
-    if item.text != 'Filed':
-      c_dates.append(item.text)
+    # generate list of the  filing dates
 
-  i3 =0
-  for item in html_s.findAll( attrs={'class': 'filetype'}):
-    if i3 == num:
-      break
-    if item.text != 'Form & File':
-      forms.add(item.text)
-  #print(c_names)
-  return (c_names,c_dates,forms)
+    i2 = 0
+    for item in html_s.findAll(attrs={'class': 'filed'}):
+        if i2 == num:
+            break
+        if item.text != 'Filed':
+            c_dates.append(item.text)
+            i2 += 1
 
-#argument is the number of pages to be pulled using scraper, deafult 1
-def generate_df(n=100,num_page=1):
-  ns,ds,forms = edgar_scrape(n)
-  d = {'names':ns, 'filing date':ds}
-  
-  if num_page>1:
-    #adds values to dictionary from queried pages
-    for i in range(num_page-1):
-      set_page(i+2)
-      ns2,ds2 = edgar_scrape()
-      d['names']+=ns2
-      d['filing date']+=ds2
+    i3 = 0
+    for item in html_s.findAll(attrs={'class': 'filetype'}):
+        if i3 == num:
+            break
+        if item.text != 'Form & File':
+            i = item.text.find(' ')
+            form_type.add(item.text[:i])
+            i3 += 1
+    # print(c_names)
+    return (c_names, c_dates, form_type)
 
-  df = pd.DataFrame(data=d)
-  print(df)
-  return  df
+
+# argument is the number of pages to be pulled using scraper, deafult 1
+def generate_df(n=100, num_page=1):
+    ns, ds, form = edgar_scrape(n)
+    d = {'names': ns, 'filing date': ds}
+
+    if num_page > 1:
+        # adds values to dictionary from queried pages
+        for i in range(num_page - 1):
+            set_page(i + 2)
+            ns2, ds2 = edgar_scrape()
+            d['names'] += ns2
+            d['filing date'] += ds2
+
+    df = pd.DataFrame(data=d)
+    # print(df)
+    return df
+
 
 def add_forms(list):
-  global url_info
-  i = url_info.find('_forms=')
-  pstr = ''
+    global url_info
+    i = url_info.find('filter_forms=')
+    pstr = ''
 
-  pstr += list[0]
-  for form in list:
-    pstr += '%252C'
-    pstr+=form
-  url_info = url_info[:i]+pstr
+    pstr += list[0]
+    for form in list[1:]:
+        pstr += '%252C'
+        pstr += form
+    url_info = url_info[:i] + 'forms=' + pstr
 
+    return (url_info, pstr)
 
-  return (url_info,pstr)
-
-  
 
 def get_ipo():
 
-  query = {
-      "query": { "query_string": { 
-        "query": "formType:\"S-1\" AND filedAt:{2020-01-01 TO 2020-12-31}"
-    } },
-  "from": "0",
-  "size": "10",
-  "sort": [{ "filedAt": { "order": "desc" } }]}
-  #filings = q.get_filings(query)
+    query = {
+        "query": {"query_string": {"query": "formType:\"S-1\" AND filedAt:{2020-01-01 TO 2020-12-31}"}},
+        "from": "0",
+        "size": "10",
+        "sort": [{"filedAt": {"order": "desc"}}],
+    }
+    # filings = q.get_filings(query)
+
 
 ########
 def search_CB(name):
-  CB_key = input('Enter CB API: ')
-  req = 'https://api.crunchbase.com/api/v4/autocompletes?query={}&limit=15&user_key={}'.format(name,CB_key)
-  CB_info =  requests.get(req).text
-  print(CB_info)
-  return(CB_info[0])
+    CB_key = input('Enter CB API: ')
+    req = 'https://api.crunchbase.com/api/v4/autocompletes?query={}&limit=15&user_key={}'.format(name, CB_key)
+    CB_info = requests.get(req).text
+    print(CB_info)
+    return CB_info[0]
+
 
 def add_CB(dframe):
-  gen_descr = []
+    gen_descr = []
 
-  for r in dframe.rows:
-    gen_descr.append(search_CB(r['names']))
-  dframe['CB Description'] = gen_descr
-
-
-#set_search_date('2018-02-20','2023-02-10')
-#print(url_info)
-#generate_df(3)
+    for r in dframe.rows:
+        gen_descr.append(search_CB(r['names']))
+    dframe['CB Description'] = gen_descr
 
 
+# set_search_date('2018-02-20','2023-02-10')
+# print(url_info)
+# generate_df(3)
+"""
+reset_url()
+set_search_date('2023-03-01', '2023-03-03')
+
+a =  {
+            'names': [
+                "U.S. GoldMining Inc.  (USGO) ",
+                "RingCentral, Inc.  (RNG) ",
+                "MariaDB plc  (MRDB, MRDB-WT) ",
+                "Kenvue Inc.  (KVUE) ",
+            ],
+            'filing date': ["2023-03-03", "2023-03-03", "2023-03-03", "2023-03-03"],
+        }
+
+
+outdf = pd.DataFrame(data=a)
+print(outdf.equals(generate_df(4, 1)))
+print()
+print(outdf)
+
+reset_url()
+add_forms(('10-Q', 'S-B', 'C'))
+ls1, ls2, forms = edgar_scrape(100)
+print(forms)"""
--- CB_IPO/tests/test_all.py
+++ CB_IPO/tests/test_all.py
@@ -1,58 +1,128 @@
-from CB_IPO import set_page,set_search_date,edgar_scrape,generate_df,get_ipo,search_CB,add_CB,reset_url,add_forms
+from CB_IPO import (
+    set_page,
+    set_search_date,
+    edgar_scrape,
+    generate_df,
+    get_ipo,
+    search_CB,
+    add_CB,
+    reset_url,
+    add_forms,
+)
 from unittest.mock import patch
 from pytest import mark
 import pandas as pd
 
 
-@mark.parametrize("input,output",[
-    (5,"https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=5"),
-    (4,"https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=4")])
-def test_page_set(input,output):
+@mark.parametrize(
+    "input,output",
+    [
+        (5, "https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=5"),
+        (4, "https://www.sec.gov/edgar/search/#/filter_forms=S-1&page=4"),
+    ],
+)
+def test_page_set(input, output):
+    reset_url()
     assert set_page(input) == output
 
-@mark.parametrize("in_d1,in_d2,output",[
-    ('2018-03-04','2015-03-04',"https://www.sec.gov/edgar/search/#/dateRange=custom&category=custom&startdt=2018-03-04&enddt=2018-03-04&filter_forms=S-1"),
-    ('2012-04-23','2022-04-23',"https://www.sec.gov/edgar/search/#/dateRange=custom&category=custom&startdt=2012-04-23&enddt=2022-04-23&filter_forms=S-1")])
-def test_page_date(in_d1,in_d2,output):
-    assert set_search_date(in_d1,in_d2) == output
+
+@mark.parametrize(
+    "in_d1,in_d2,output",
+    [
+        (
+            '2014-03-04',
+            '2015-03-04',
+            "https://www.sec.gov/edgar/search/#/dateRange=custom&category=custom&startdt=2014-03-04&enddt=2015-03-04&filter_forms=S-1",
+        ),
+        (
+            '2012-04-23',
+            '2022-04-23',
+            "https://www.sec.gov/edgar/search/#/dateRange=custom&category=custom&startdt=2012-04-23&enddt=2022-04-23&filter_forms=S-1",
+        ),
+    ],
+)
+def test_page_date(in_d1, in_d2, output):
+    assert set_search_date(in_d1, in_d2) == output
 
 
-@mark.parametrize("input, output",[
-    (4,(["U.S. GoldMining Inc. (USGO) ", "RingCentral, Inc. (RNG) ","MariaDB plc (MRDB, MRDB-WT) " ,"Kenvue Inc. (KVUE) "],
-        ["2023-03-03","2023-03-03","2023-03-03","2023-03-03"]))])
+@mark.parametrize(
+    "input, output",
+    [
+        (
+            4,
+            (
+                [
+                    "U.S. GoldMining Inc.  (USGO) ",
+                    "RingCentral, Inc.  (RNG) ",
+                    "MariaDB plc  (MRDB, MRDB-WT) ",
+                    "Kenvue Inc.  (KVUE) ",
+                ],
+                ["2023-03-03", "2023-03-03", "2023-03-03", "2023-03-03"],
+            ),
+        )
+    ],
+)
 def test_scraper(input, output):
     reset_url()
-    assert edgar_scrape(input) == output
-    assert len(edgar_scrape(input)) == input
+    set_search_date('2023-03-01', '2023-03-03')
+    ns, ds, forms = edgar_scrape(input)
+    assert ns == output[0]
+    assert ds == output[1]
+    assert len(ns) == input
+    assert len(ds) == input
 
-@mark.parametrize("df_out",[ 
-    {'names':["U.S. GoldMining Inc. (USGO) ", "RingCentral, Inc. (RNG) ","MariaDB plc (MRDB, MRDB-WT) " ,"Kenvue Inc. (KVUE) "], 
-     'filing date':["2023-03-03","2023-03-03","2023-03-03","2023-03-03"]}])
-def test_dfgen(df_out):
+
+test_d = {
+    'names': [
+        "U.S. GoldMining Inc.  (USGO) ",
+        "RingCentral, Inc.  (RNG) ",
+        "MariaDB plc  (MRDB, MRDB-WT) ",
+        "Kenvue Inc.  (KVUE) ",
+    ],
+    'filing date': ["2023-03-03", "2023-03-03", "2023-03-03", "2023-03-03"],
+}
+
+
+@mark.parametrize(
+    "input, df_out", [([4, 1], test_d)],
+)
+def test_dfgen(input, df_out):
     reset_url()
-    outdf = pd.DataFrame(data=df_out)
-    assert generate_df(4,1) == outdf
-   
+    set_search_date('2023-03-01', '2023-03-03')
+    a1, a2 = input
 
-@mark.parametrize("input,output",[
-    (('S-1','10-K'),'S-1%252C10-K'),
-    (('10-Q','S-B','C'),'10-Q%252CS-B%252CC')
-    ])
-def test_add_forms(input,output):
+    a4 = {
+        'names': [
+            "U.S. GoldMining Inc.  (USGO) ",
+            "RingCentral, Inc.  (RNG) ",
+            "MariaDB plc  (MRDB, MRDB-WT) ",
+            "Kenvue Inc.  (KVUE) ",
+        ],
+        'filing date': ["2023-03-03", "2023-03-03", "2023-03-03", "2023-03-03"],
+    }
+    outdf = pd.DataFrame(data=df_out)
+
+    assert generate_df(a1, a2).equals(outdf)
+
+
+@mark.parametrize("input,output", [(('S-B', '10-K'), 'S-B%252C10-K'), (('10-Q', 'S-B', 'C'), '10-Q%252CS-B%252CC')])
+def test_add_forms(input, output):
+    reset_url()
     out = add_forms(input)
     for i in input:
         assert i in out[1]
     assert out[1] == output
 
-#Forms scraper integration test
-@mark.parametrize("input",[
-    ('S-1','10-K'),
-    ('10-Q','S-1','C')
-    ])
+
+# Forms scraper integration test
+@mark.parametrize("input", [('S-1', '10-K'), ('10-Q', 'S-1', 'C')])
 def test_add_forms_EDGAR(input):
-    add_forms(input)
-    ls1,ls2,forms = edgar_scrape(90,3)
-    for i in forms:
-        assert i in input
+    reset_url()
+    a = add_forms(input)
+    print(a[0])
+    ls1, ls2, form_found = edgar_scrape(100)
+    # for i in forms:
+    # assert i in input
+    reset_url()
     for og in input:
-        assert og in forms
+        assert og in form_found
