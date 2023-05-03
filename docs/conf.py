9ca383a07a4785cde2e404f2c3bee5bbcbfe34f4
Leon Wu <leonwu@Leons-MacBook-Air-3.local>
2023-05-03 18:48:02 -0400

    Added new values to scrape for from 10k document, added functionality for calculating financial ratios
    added summarization functionality for 10k, updated tests
    
    fixed linting issues
    
    fixed docstrings
    
    fixed linting issue
    
    bumped version, added tests, updated documentation, added summary features
    
    linted tests
    
    reformatted issue in tests
    
    linted issues

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2023, Leon Wu'
 author = 'Leon Wu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
